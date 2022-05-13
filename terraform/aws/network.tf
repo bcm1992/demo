70f38d616dc90b0aeb8b7b51b9c9b3f0a51101e1
Jun Makino <jun.makino@gmail.com>
2022-05-13 18:29:10 +0000

    update aws

---
+++ terraform/aws/network.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.10.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.10.0.0/24"
+  map_public_ip_on_launch = true
+
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "gw" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+  depends_on = ["aws_internet_gateway.gw"]
+}
